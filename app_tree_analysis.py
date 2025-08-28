c6b073da2bbcec3ce79bb835c14eb26a39ec44bb
Luci-sdsd <luci.jiang12@gmail.com>
2025-08-28 10:23:08 +0200

    Update app_tree_analysis.py

--- app_tree_analysis.py
+++ app_tree_analysis.py
@@ -22,8 +22,8 @@ try:
     #     azure_ad_token_provider=token_provider,
     # )
     client = AzureOpenAI(
-        azure_endpoint = st.secrets["auth_endpoint"] # os.getenv("AZURE_OPENAI_ENDPOINT_SWEDEN_CENTRAL"), 
-        api_key= st.secrets["auth_key"]# os.getenv("AZURE_OPENAI_API_KEY_SWEDEN_CENTRAL"),  
+        azure_endpoint = st.secrets["auth_endpoint"], # os.getenv("AZURE_OPENAI_ENDPOINT_SWEDEN_CENTRAL"), 
+        api_key= st.secrets["auth_key"], # os.getenv("AZURE_OPENAI_API_KEY_SWEDEN_CENTRAL"),  
         api_version= st.secrets["auth_version"] # os.getenv("AZURE_OPENAI_API_VERSION")
       )
     if not os.getenv("AZURE_OPENAI_ENDPOINT_SWEDEN_CENTRAL"):
